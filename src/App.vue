396470568e588fb8895d4c9f4761fc815f389691
ms143014 <664089244@qq.com>
2023-01-04 00:21:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Vue3</h1>
+    <p>{{ msg }}</p>
+    <button @click="changeMsg">Change Msg</button>
+</template>
+<script>
+//vue3
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    const msg = ref('Hello Vue3')
+    const changeMsg = () => {
+      msg.value = 'Hello Vue3.1' + new Date().getTime()
+    }
+    return {
+      msg,
+      changeMsg
+    }
+  }
+})
+</script>
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+//vite config
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [Vue()],
+})
